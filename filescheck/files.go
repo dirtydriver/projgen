a30226a32c9c0f480a64cbac626d28615ad70077
Petruska Ákos <petruskaakos@gmail.com>
2025-02-13 16:33:41 +0100

    feat: implement directory functions

--- filescheck/files.go
+++ filescheck/files.go
@@ -1,16 +1,17 @@
 package filescheck
 
 import (
-	"errors"
 	"fmt"
-	"io/fs"
 	"os"
 	"path/filepath"
 )
 
-func filesInDirectories(dir string) ([]string, error) {
+func FilesInDirectories(dir string) ([]string, error) {
 	var files []string
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 		if !info.IsDir() {
 			files = append(files, path)
 		}
@@ -21,45 +22,42 @@ func filesInDirectories(dir string) ([]string, error) {
 
 }
 
-func copyFiles(files []string, target_dir string) error {
+func CopyFiles(files []string, targetDir string) error {
 
 	for _, file := range files {
 		input, err := os.ReadFile(file)
 		if err != nil {
 			return fmt.Errorf("reading file %q: %w", file, err)
 		}
-
-		if err := os.WriteFile(target_dir, input, 0644); err != nil {
+		destPath := filepath.Join(targetDir, filepath.Base(file))
+		if err := os.WriteFile(destPath, input, 0644); err != nil {
 			return err
 		}
 
-		return err
 	}
+	return nil
 }
 
-func createDirectory(dir_name string) error {
-
-	isDirExists := isDirectoryExists(dir_name)
-	if isDirExists {
-		return error.Error("The directory already exists")
-	}
-
-	if err := os.MkdirAll(dir_name, os.ModePerm); err != nil {
+func CreateDirectory(dirName string) error {
+	if info, err := os.Stat(dirName); err == nil {
+		if info.IsDir() {
+			return fmt.Errorf("the directory already exists")
+		}
+		// If the path exists but isn't a directory, you might want to handle that differently.
+		return fmt.Errorf("a non-directory file exists at %s", dirName)
+	} else if !os.IsNotExist(err) {
+		// An unexpected error occurred
 		return err
 	}
 
+	// The directory does not exist, so create it.
+	return os.MkdirAll(dirName, os.ModePerm)
 }
-func isDirectoryExists(path string) bool {
-	_, err := os.Stat(path)
 
-	if err == nil {
-		return true
-	}
-
-	if errors.Is(err, fs.ErrNotExist) {
+func IsDirectoryExists(path string) bool {
+	info, err := os.Stat(path)
+	if err != nil {
 		return false
 	}
-
-	return false
-
+	return info.IsDir()
 }
