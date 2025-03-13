4880ee5d3ad8bb9edc09f030fe874146ee71451d
Petruska Ákos <petruskaakos@gmail.com>
2025-03-13 18:23:48 +0100

    feat: restructure commandline solution

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -10,6 +10,8 @@ import (
 	"github.com/dirtydriver/projgen/filescheck"
 	"github.com/dirtydriver/projgen/project"
 	"github.com/dirtydriver/projgen/templater"
+	"github.com/dirtydriver/projgen/utils"
+	"github.com/dirtydriver/projgen/version"
 	"github.com/spf13/cobra"
 )
 
@@ -18,42 +20,40 @@ var (
 	projectName    string
 	outputDir      string
 	parameters     []string
-	getTemplate    bool
-	templateFile   string
 	templateDir    string
 	parametersFile string
 )
 
 func getRootCmd() *cobra.Command {
-
 	rootCmd := &cobra.Command{
 		Use:   "projgen",
 		Short: "Project generator that renders project skeletons from templates",
+	}
+
+	// Add shared flags that apply to multiple commands
+	rootCmd.PersistentFlags().StringVar(&templateDir, "template-dir", "", "Path to the template directory")
+	rootCmd.PersistentFlags().StringVarP(&projectType, "type", "t", "", "Type of project (e.g. maven, gradle, angular)")
+
+	// Mark required persistent flags
+	rootCmd.MarkPersistentFlagRequired("template-dir")
+	rootCmd.MarkPersistentFlagRequired("type")
+
+	// Add all subcommands
+	rootCmd.AddCommand(
+		getVersionCmd(),
+		getGenerateCmd(),
+		getInspectCmd(),
+	)
+
+	return rootCmd
+}
+
+func getGenerateCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "generate",
+		Short: "Generate a new project from a template",
 		Run: func(cmd *cobra.Command, args []string) {
-			// If user wants to see the expected template parameters.
-			if getTemplate {
-				if projectType == "" && templateDir == "" {
-					log.Fatal("Please provide a project type and template directory")
-				}
-				templatePath := path.Join(templateDir, projectType)
-				files, err := filescheck.FindTemplateFiles(templatePath, "tmpl")
-				if err != nil {
-					log.Fatalf("Error collection template files: %v", err)
-				}
-
-				params, err := templater.CollectParameters(files)
-				if err != nil {
-					log.Fatalf(err.Error())
-				}
-
-				fmt.Println("Template requires the following parameters:")
-				for _, p := range params {
-					fmt.Println(" -", p)
-				}
-				return
-			}
-
-			// Collect additional parameters passed via --parameter flags.
+			// Collect additional parameters passed via --parameter flags
 			paramsMap := make(map[string]interface{})
 			for _, p := range parameters {
 				parts := strings.SplitN(p, "=", 2)
@@ -70,23 +70,28 @@ func getRootCmd() *cobra.Command {
 				}
 			}
 
-			_, NameExists := paramsMap["name"]
-			_, TypeExists := paramsMap["type"]
-
-			// Optionally, you can also merge explicit flags (like projectType or projectName)
-			// into the parameters map if you want them to be available in the template.
-			if projectName != "" && !NameExists {
+			// Add projectName to params if provided
+			if projectName != "" {
 				paramsMap["name"] = projectName
 			}
-			if projectType != "" && !TypeExists {
-				paramsMap["type"] = projectType
+
+			templatePath := path.Join(templateDir, projectType)
+			files, err := filescheck.FindTemplateFiles(templatePath, "tmpl")
+			if err != nil {
+				log.Fatalf("Error collecting template files: %v", err)
 			}
 
-			// Proceed with generating the project using the merged parameters.
-			if projectType == "" && !NameExists || projectName == "" && !TypeExists {
-				log.Fatal("Project type and project name are required")
+			params, err := templater.CollectParameters(files)
+			if err != nil {
+				log.Fatalf(err.Error())
 			}
-			err := project.Generate(templateDir, outputDir, paramsMap)
+
+			err = utils.CheckMissingKeys(paramsMap, params)
+			if err != nil {
+				log.Fatalf(err.Error())
+			}
+
+			err = project.Generate(templatePath, outputDir, paramsMap)
 			if err != nil {
 				log.Fatalf("Error generating project: %v", err)
 			}
@@ -94,30 +99,56 @@ func getRootCmd() *cobra.Command {
 		},
 	}
 
-	// Basic project flags.
-	rootCmd.Flags().StringVarP(&projectType, "type", "t", "", "Type of project (e.g. maven, gradle, angular)")
-	rootCmd.Flags().StringVarP(&projectName, "name", "n", "", "Name of the project")
-	rootCmd.Flags().StringVarP(&outputDir, "out", "o", ".", "Output directory")
-	rootCmd.Flags().StringVarP(&parametersFile, "file", "f", "", "Path to the parameters file to inspect")
+	// Add flags specific to generate command
+	cmd.Flags().StringVarP(&projectName, "name", "n", "", "Name of the project (can also be provided via --parameter name=value)")
+	cmd.Flags().StringVarP(&outputDir, "out", "o", ".", "Output directory")
+	cmd.Flags().StringVarP(&parametersFile, "file", "f", "", "Path to the parameters file")
+	cmd.Flags().StringArrayVarP(&parameters, "parameter", "p", []string{}, "Additional parameters in key=value format")
 
-	// Parameter flags: allow multiple values.
-	rootCmd.Flags().StringArrayVarP(&parameters, "parameter", "p", []string{}, "Additional parameters in key=value format")
-
-	// Flags to list expected template parameters.
-	rootCmd.Flags().BoolVar(&getTemplate, "get-template-params", false, "List the parameters required by the template")
-	rootCmd.Flags().StringVar(&templateDir, "template-dir", "", "Path to the template directory")
-	rootCmd.Flags().StringVar(&templateFile, "template-file", "", "Path to the template file to inspect")
-
-	return rootCmd
+	return cmd
 }
 
-// RunRootCmd executes the root command of the RepoForge CLI tool.
+func getInspectCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "inspect",
+		Short: "Inspect template parameters and requirements",
+		Run: func(cmd *cobra.Command, args []string) {
+			templatePath := path.Join(templateDir, projectType)
+			files, err := filescheck.FindTemplateFiles(templatePath, "tmpl")
+			if err != nil {
+				log.Fatalf("Error collecting template files: %v", err)
+			}
+
+			params, err := templater.CollectParameters(files)
+			if err != nil {
+				log.Fatalf(err.Error())
+			}
+
+			fmt.Println("Template requires the following parameters:")
+			for _, p := range params {
+				fmt.Println(" -", p)
+			}
+		},
+	}
+}
+
+func getVersionCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "version",
+		Short: "Print the current version of projgen",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("projgen version %s\n", version.Version)
+		},
+	}
+}
+
+// RunRootCmd executes the root command of the projgen CLI tool.
 // If no arguments are provided, it displays the help information.
 // The function handles command execution and exits with an error if the command fails.
 func RunRootCmd() {
 	rootCmd := getRootCmd()
 	if len(os.Args) < 2 {
-		// Print help and exit if no flags or arguments are provided.
+		// Print help and exit if no flags or arguments are provided
 		_ = rootCmd.Help()
 		os.Exit(0)
 	}
